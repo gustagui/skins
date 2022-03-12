8ff6e49a2094f4df8534ac47390d4a016225ba0a
Gustavo <gustagui.dev@gmail.com>
2022-03-12 00:39:31 -0300

    js/css

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,7 @@
-.
+const card = document.querySelector('#img-card');
+
+
+// card.addEventListener('click', () => {
+//     card.style.transform = 'scale(1.5)';
+//     card.style.cursor = "pointer";
+// });
